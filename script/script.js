f967a5de8ab9f8e13b811245f2611af93fe5c54f
elham rajaee <elham.rajairad@gmail.com>
2022-03-22 16:30:42 +0430

    add top smoothe scroll

--- script/script.js
+++ script/script.js
@@ -4,3 +4,21 @@ window.addEventListener("scroll", function () {
   var header = document.querySelector("#header");
   header.classList.toggle("sticky", window.scrollY > 0);
 });
+
+// top button
+var mybutton = document.getElementById("myBtn");
+
+window.onscroll = function() {scrollFunction()};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+function topFunction() {
+  document.body.scrollTop = 0;
+  document.documentElement.scrollTop = 0;
+}
