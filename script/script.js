1060f9328700399b2aa1d100f508716c8322f015
elham rajaee <elham.rajairad@gmail.com>
2022-04-05 18:49:05 +0430

    add page item

--- script/script.js
+++ script/script.js
@@ -3,12 +3,15 @@
 window.addEventListener("scroll", function () {
   var header = document.querySelector("#header");
   header.classList.toggle("sticky", window.scrollY > 0);
+  header.classList.add("header__color");
 });
 
 // top button
 var mybutton = document.getElementById("myBtn");
 
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function () {
+  scrollFunction();
+};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
@@ -21,4 +24,18 @@ function scrollFunction() {
 function topFunction() {
   document.body.scrollTop = 0;
   document.documentElement.scrollTop = 0;
+}
+
+function openCity(evt, cityName) {
+  var i, tabcontent, tablinks;
+  tabcontent = document.getElementsByClassName("tabcontent");
+  for (i = 0; i < tabcontent.length; i++) {
+    tabcontent[i].style.display = "none";
+  }
+  tablinks = document.getElementsByClassName("tablinks");
+  for (i = 0; i < tablinks.length; i++) {
+    tablinks[i].className = tablinks[i].className.replace(" active", "");
+  }
+  document.getElementById(cityName).style.display = "block";
+  evt.currentTarget.className += " active";
 }
