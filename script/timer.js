0c476f20cf6df9c690b7c02122689fb4ebcf6e86
elham rajaee <elham.rajairad@gmail.com>
2022-05-14 15:07:00 +0430

    add timer to card

--- script/timer.js
+++ script/timer.js
@@ -3,5 +3,7 @@
 setInterval(myTime, 1000);
 function myTime() {
   let myDate = new Date();
-  document.querySelector(".timer").textContent = myDate.toLocaleTimeString();
+  document.querySelectorAll(".timer").forEach(function (i) {
+    i.textContent = myDate.toLocaleTimeString();
+  });
 }
